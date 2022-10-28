a2de7ef17f7a219245ce9329fdace4ab3bc90327
Ivana Lazarevic <93544830+lazarevicivana@users.noreply.github.com>
2022-10-28 20:42:03 +0200

    generated api clients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
@@ -7,6 +7,7 @@ import { MaterialModule } from "./material/material.module";
 import { HospitalModule } from "./modules/hospital/hospital.module";
 import { PagesModule } from "./modules/pages/pages.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DoctorClient} from "./api/api-reference";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PagesModule,
     HospitalModule
   ],
-  providers: [],
+  providers: [
+    DoctorClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
