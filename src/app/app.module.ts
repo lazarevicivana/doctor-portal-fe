073dec4cf43ca3681329174712cfa2610d0adeb7
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-11-01 16:39:30 +0100

    warning resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
@@ -7,13 +7,13 @@ import { MaterialModule } from "./material/material.module";
 import { HospitalModule } from "./modules/hospital/hospital.module";
 import { PagesModule } from "./modules/pages/pages.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AppointmentClient, DoctorClient} from "./api/api-reference";
+import {AppointmentClient, DoctorClient, PatientClient} from "./api/api-reference";
 import { BodyComponent } from './components/body/body.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { AppointmentPreviewComponent } from './components/dashboard/appointment-preview/appointment-preview.component';
 import {ScheduleModule} from "./modules/schedule/schedule.module";
 import {LoginModule} from "./login/login/login.module";
 import {DashboardModule} from "./components/dashboard/dashboard.module";
+
 
 @NgModule({
   declarations: [
@@ -30,12 +30,13 @@ import {DashboardModule} from "./components/dashboard/dashboard.module";
     PagesModule,
     HospitalModule,
     ScheduleModule,
-    LoginModule
+    LoginModule,
     DashboardModule
   ],
   providers: [
     DoctorClient,
-    AppointmentClient
+    AppointmentClient,
+    PatientClient
   ],
   exports: [
   ],
