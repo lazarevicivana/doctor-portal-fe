21efbd899eeadd9e3c072b4d1a42fbf9ccc914e6
Dejan Gloginjic <deki555@hotmail.com>
2022-11-01 16:19:18 +0100

    Form validation

--- src/app/modules/hospital/bloodbank-registration/bloodbank-registration.component.ts
+++ src/app/modules/hospital/bloodbank-registration/bloodbank-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BloodBank } from '../model/bloodBank.model';
 import { BloodBankService } from '../services/blood-bank.service';
@@ -12,17 +13,30 @@ export class BloodbankRegistrationComponent{
 
   public bloodBank: BloodBank = new BloodBank();
 
+  public registrationForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    serverAddress: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.email, Validators.required])
+  })
+
   constructor(private bloodBankService: BloodBankService, private router: Router) { }
 
+  get name(){
+    return this.registrationForm.get('name')
+  }
+
+  get serverAddress(){
+    return this.registrationForm.get('serverAddress')
+  }
+
+  get email(){
+    return this.registrationForm.get('email')
+  }
+
   public createBloodBank() {
-    if (!this.isValidInput()) return;
     this.bloodBankService.createBloodBank(this.bloodBank).subscribe(res => {
       console.log("Success!")
     });
-  }
-
-  private isValidInput(): boolean {
-    return this.bloodBank?.name != '' && this.bloodBank?.serverAddress != '' && this.bloodBank?.email != '';
   }
 
 
