89a96eb57cc8dcf4564ef344be7921d07146b43b
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-11-02 21:24:28 +0100

    feature: reformation of code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,40 +6,24 @@ import { MaterialModule } from "./material/material.module";
 import { HospitalModule } from "./modules/hospital/hospital.module";
 import { PagesModule } from "./modules/pages/pages.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-
-
 import { AppComponent } from "./app.component";
-
 import {AppointmentClient, DoctorClient, PatientClient, ScheduleClient} from "./api/api-reference";
-
 import { BodyComponent } from './components/body/body.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import {ScheduleModule} from "./modules/schedule/schedule.module";
 import {LoginModule} from "./login/login/login.module";
 import {DashboardModule} from "./components/dashboard/dashboard.module";
-import { DatePickerComponent } from './components/date-picker/date-picker.component';
-
-
 import { RescheduleAppointmentComponent } from './components/reschedule-appointment/reschedule-appointment.component';
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {ReactiveFormsModule} from "@angular/forms";
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
     SidenavComponent,
-
-    RescheduleAppointmentComponent,
-
-    DatePickerComponent
-
+    RescheduleAppointmentComponent
   ],
     imports: [
         BrowserModule,
@@ -49,10 +33,7 @@ import {ReactiveFormsModule} from "@angular/forms";
         MaterialModule,
         PagesModule,
         HospitalModule,
-
         HttpClientModule,
-
-
         ScheduleModule,
         LoginModule,
         DashboardModule,
