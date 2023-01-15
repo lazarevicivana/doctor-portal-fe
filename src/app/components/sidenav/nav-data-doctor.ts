03df6825300c9b6989fd9661eaaea18ac8146f1c
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2023-01-15 18:25:08 +0100

    notifications update

--- src/app/components/sidenav/nav-data-doctor.ts
+++ src/app/components/sidenav/nav-data-doctor.ts
@@ -2,46 +2,55 @@ export const navbarDataDoctor=[
   {
     routeLink: 'dashboard',
     icon: 'fal fa-home',
-    label: 'Dashboard'
+    label: 'Dashboard',
+    notification: false
   },
   {
     routeLink: 'consiliums',
     icon: 'fal fa-calendar',
-    label: 'Calendar'
+    label: 'Calendar',
+    notification: false
   },
   {
     routeLink: 'your-patients',
     icon: 'fal fa-users',
-    label: 'Your patients'
+    label: 'Your patients',
+    notification: false
   },
   {
     routeLink: 'blood-units',
     icon: 'fal fa-tint',
-    label: 'Blood units'
+    label: 'Blood units',
+    notification: false
   },
   {
     routeLink: 'hospitalizes-patients',
     icon: 'fal fa-solid fa-bed',
-    label: 'Hospitalize'
+    label: 'Hospitalize',
+    notification: false
   },
   {
     routeLink: 'doctors-holidays',
     icon: 'fal fa-sleigh',
-    label: 'Your holidays'
+    label: 'Your holidays',
+    notification: false
   },
   {
     routeLink: 'examination-search',
     icon: 'fal  fa-clipboard-user',
-    label: 'Search'
+    label: 'Search',
+    notification: false
   },
   {
     routeLink: 'notifications',
     icon: 'fal fa-envelope',
-    label: 'Notifications'
+    label: 'Notifications',
+    notification: true
   },
   {
     routeLink: 'sign-out',
     icon: 'fal fa-sign-out',
-    label: 'Sign out'
+    label: 'Sign out',
+    notification: false
   }
 ];
